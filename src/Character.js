5c332c33a2b5bf3b4be71a125b3a0486be84830d
M0r0zKa <102826244+M0r0zKa@users.noreply.github.com>
2022-06-20 23:02:58 +0300

    new

---
+++ src/Character.js
@@ -0,0 +1,11 @@
+function Character(props) {
+    let {name,desc,picture} = props
+    return <div>
+        <h2>{name}</h2>
+        <p>{desc}</p>
+        <img src={picture}/>
+    </div>
+
+}
+
+export default Character
